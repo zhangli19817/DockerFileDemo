3427909b047594eb8639befd07be3751c15fd661
luffyzhang <zhangli19817@gmail.com>
2020-02-28 18:54:03 +0800

    msg

---
+++ http-client.go
@@ -0,0 +1,17 @@
+package main
+import (
+"net/http"
+)
+import "io/ioutil"
+import "fmt"
+func main() {
+url := "https://raw.githubusercontent.com/" +
+"dockerinaction/ch8_multi_stage_build/master/http-client.go"
+resp, err := http.Get(url)
+if err != nil {
+panic(err)
+}
+defer resp.Body.Close()
+body, err := ioutil.ReadAll(resp.Body)
+fmt.Println("response:\n", string(body))
+}
